c6e311de390673e005b88df68d8d7f34c378afbc
Sudaraka Pieris <spie2381@uni.sydney.edu.au>
2024-07-31 10:00:50 +1000

    trying to make bat and sh files to run the programs

---
+++ main.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Installing dependencies...
+pip install -r requirements.txt
+
+echo Running the program...
+python main.py
+
+pause
---
+++ main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Running the program..."
+python main.py
